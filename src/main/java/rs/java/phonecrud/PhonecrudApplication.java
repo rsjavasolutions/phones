00b4ce39ee3511b566282ae0ff28097915442668
rsjavasolutions <stan.robert.1986@gmail.com>
2020-03-20 10:21:19 +0100

    New project

---
+++ src/main/java/rs/java/phonecrud/PhonecrudApplication.java
@@ -0,0 +1,13 @@
+package rs.java.phonecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/java/phonecrud/PhonecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.java.phonecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
