0c0eb077e458c0ea81ec2b34daaf056eb40317dd
rsjavasolutions <stan.robert.1986@gmail.com>
2020-03-20 10:33:13 +0100

    Created PhoneRepository class

--- src/test/java/rs/java/phonecrud/repository/PhoneRepository.java
+++ src/test/java/rs/java/phonecrud/repository/PhoneRepository.java
@@ -1,6 +1,7 @@
 package rs.java.phonecrud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import rs.java.phonecrud.model.Phone;
 
-public interface PhoneRepository extends JpaRepository {
+public interface PhoneRepository extends JpaRepository <Phone, Integer> {
 }
